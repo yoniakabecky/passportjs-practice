878775472e6e8c61af95ee4bea58dde96f14a6f4
Yoni <yoniakabecky@gmail.com>
2020-09-21 16:48:23 -0700

    #3 - Settingup an Express App

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+// set up view engine
+app.set("view engine", "ejs");
+
+// create home route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("app now listening for requests on port 3000 🚀");
+});
